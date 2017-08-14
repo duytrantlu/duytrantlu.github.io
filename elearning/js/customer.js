430707bf43ea772435163b35ac0138edc09631d4
duytrantlu <duytrinhmi@gmail.com>
2017-08-14 10:48:03 +0700

    chane-profile

--- elearning/js/customer.js
+++ elearning/js/customer.js
@@ -150,7 +150,7 @@ $(document).ready(function(){
 	      videoHeight: -1
 	    });*/
 	//function xem thông tin chi tiết giảng viên
-	call_intructor_popup()
+	
 	 //xử lý faqs Down Up
 	$('.faq-item').hover(function() {
     	$(this).children('.faq2').stop().slideDown();
@@ -385,35 +385,5 @@ function call_pop_lecture_preview() {
       });
   });
 }
-//function xem chi tiết thông tin giảng viên
-function call_intructor_popup() {
-	$('a.call-intructor-popup').addClass('fancybox.ajax').fancybox({
-	    maxWidth: 650,
-	    minWidth: 300,
-	    width: '90%',
-	    fitToView: false,
-	    autoSize: false,
-	    autoHeight: true,
-	    closeClick: false,
-	    openEffect: 'none',
-	    closeEffect: 'elastic',
-	    padding: 0,
-	    closeBtn: false,
-	    afterLoad: function(html) {
-	      $('.fancybox-outer>.instructor-avatar').remove();
-	      newavatar = $(html.content).find('.instructor-avatar');
-	      newavatar.appendTo($('.fancybox-outer'));
-	    },
-	    helpers: {
-	      overlay: {
-	        css: {
-	          'background': 'rgba(255,255,255,0.6)'
-	        }
-	      }
-	    },
-	    beforeShow: function() {
-	      $('.fancybox-wrap').data('animation', 'fadeInDown');
-	      runAnim($('.fancybox-wrap'));
-	    }
-	  });
-}
+
+
