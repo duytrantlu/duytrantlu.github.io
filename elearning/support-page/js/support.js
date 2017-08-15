214cc2b082e0a6192197d6cc5faf8b8893b69565
duytrantlu <duytrinhmi@gmail.com>
2017-08-15 23:48:16 +0700

    add-backtotop

--- elearning/support-page/js/support.js
+++ elearning/support-page/js/support.js
@@ -8,5 +8,29 @@
 			
 			$('.navbar-inverse').removeClass('after-scroll-wrap');
 		}			
-
 	});
+/**** GLOBAL :: SCROLL PAGE UP  ****/
+	if ( (jQuery(window).width()>=767) ) {
+		// hide #back-top first
+		jQuery("#scrollbkToTop").hide();
+		jQuery( "body" ).append( "<p id=\"scrollbkToTop\" style=\"display: none;\"><a href=\"#top\"><span> <i class=\"fa fa-arrow-up footer-go-uplink\"></i></span></a></p>" );
+		
+		// fade in #back-top
+		jQuery(function () {
+			jQuery(window).on("scroll",function() {
+				if (jQuery(this).scrollTop() > 150) {
+					jQuery('#scrollbkToTop').fadeIn();
+				} else {
+					jQuery('#scrollbkToTop').fadeOut();
+				}
+			});
+		
+			// scroll body to 0px on click
+			jQuery('#scrollbkToTop a').on("click",function() { 
+				jQuery('body,html').animate({
+					scrollTop: 0
+				}, 800);
+				return false;
+			});
+		});
+	}
